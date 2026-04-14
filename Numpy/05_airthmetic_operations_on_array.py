0c6f411a2ce693e64f2a40edccd298fcb09c42c1
Manvendra <manvendrajhansi1406@gmail.com>
2026-04-15 03:09:43 +0530

    operations in array

---
+++ Numpy/05_airthmetic_operations_on_array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+n1=np.array([[1,2,3,4],[1,2,3,4]])
+n2=np.array([[1,2,3,4],[1,2,3,4]])
+
+# airthmetic operation on array
+n3=np.multiply(n1,n2)
+print(n3)
+
+# scalar airthmetics operation on array's elements
+
+n4=n1**3
+print(n4)
+
